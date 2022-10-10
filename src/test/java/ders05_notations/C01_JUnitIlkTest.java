70c8ceffbd50ad03cf92d860b04f03ee474cd6d0
selmanbb <nurkasabali45@gmail.com>
2022-10-10 21:55:14 +0200

    JUnitIlkTest

---
+++ src/test/java/ders05_notations/C01_JUnitIlkTest.java
@@ -0,0 +1,19 @@
+package ders05_notations;
+
+import org.junit.Test;
+
+public class C01_JUnitIlkTest {
+
+    @Test
+    public void test01(){
+        System.out.println("test01");
+    }
+    @Test
+    public void test02(){
+        System.out.println("test02");
+    }
+    @Test
+    public void test03(){
+        System.out.println("test03");
+    }
+}
