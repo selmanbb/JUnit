4a7125c6cd85986239ebe1a86876291f3e089359
selmanbb <nurkasabali45@gmail.com>
2022-10-23 23:01:07 +0200

    odev2

--- src/test/java/odev2/Actionsodev2.java
+++ src/test/java/odev2/Actionsodev2.java
@@ -1,5 +1,6 @@
 package odev2;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -13,17 +14,23 @@ public class Actionsodev2 extends TestBase {
         //1- Bir Class olusturalim KeyboardActions2
         //2- https://html.com/tags/iframe/ sayfasina gidelim
         driver.get("https://html.com/tags/iframe/");
-        //3- video’yu gorecek kadar asagi inin
-        WebElement videoElementi= driver.findElement(By.xpath("//iframe[@id='skimlinks-pixels-iframe']"));
-        bekle(3);
 
-        driver.switchTo().frame(videoElementi);
-        bekle(3);
-        Actions actions=new Actions(driver);
-        actions.keyDown(Keys.ARROW_DOWN);
-        bekle(3);
+        //3- video’yu gorecek kadar asagi inin
+        WebElement iframeElementi= driver.findElement(By.xpath("//iframe[@src='https://www.youtube.com/embed/owsfdh4gxyc']"));
+
+       Actions actions=new Actions(driver);
+       actions.moveToElement(iframeElementi).perform();
+        bekle(2);
+
 
         //4- videoyu izlemek icin Play tusuna basin
+        driver.switchTo().frame(iframeElementi);
+        driver.findElement(By.xpath("//button[@class='ytp-large-play-button ytp-button ytp-large-play-button-red-bg']")).click();
+        bekle(2);
+
         //5- videoyu calistirdiginizi test edin
+        WebElement oynatButonuElementi=driver.findElement(By.xpath("//button[@aria-label='Oynat']"));
+
+        Assert.assertFalse(oynatButonuElementi.isDisplayed());
     }
 }
